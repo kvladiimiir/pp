efa2160a81da29b3132d475d8f108986dbe1484c
Vladimir <kozyrev-vova@mail.ru>
2020-03-10 22:19:07 +0300

    add lab3

--- NewThreadNumbers/NewThreadNumbers.cpp
+++ NewThreadNumbers/NewThreadNumbers.cpp
@@ -3,22 +3,43 @@
 
 #include "pch.h"
 #include <windows.h>
+#include <time.h>
 #include <string>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+clock_t start;
+ofstream fileOutput1("t1.log");
+ofstream fileOutput2("t2.log");
 
 DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 {
-	std::cout << *(int*)lpParam << " number thread\n";
+
+	for (int i = 0; i < 125; i++)
+	{
+		clock_t stop = clock();
+		if (*(int*)lpParam == 1)
+		{
+			fileOutput1 << ((float)stop - start) / CLOCKS_PER_SEC << '\n';
+		}
+		else
+		{
+			fileOutput2 << ((float)stop - start) / CLOCKS_PER_SEC << '\n';
+		}
+	}
+
 	ExitThread(0);
 }
 
 int main(int argc, char* argv[])
 {
-	int threadsNumber = std::stoi(argv[1]);
+	int threadsNumber = 2;
 	HANDLE* handles = new HANDLE[threadsNumber];
 	int* num = new int[threadsNumber];
 	for (int i = 0; i < threadsNumber; i++)
 	{
+		start = clock();
 		num[i] = i + 1;
 		handles[i] = CreateThread(NULL, 0, &ThreadProc, (LPVOID)&num[i], CREATE_SUSPENDED, NULL);
 		ResumeThread(handles[i]);
